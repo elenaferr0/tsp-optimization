8012d8bb31d890c4245898f2c5472712f45d0c37
elenaferr0 <elena.ferro@gmail.com>
2025-04-15 22:25:38 +0200

    fix: computing cost based on euclidean distance

--- part_one/src/graph/graph.cpp
+++ part_one/src/graph/graph.cpp
@@ -5,20 +5,15 @@
 
 using namespace std;
 
-Graph::Graph(const string& file_path)
-{
+Graph::Graph(const string &file_path) {
     fstream file(file_path);
-    if (!file.is_open())
-    {
+    if (!file.is_open()) {
         throw runtime_error("Could not open file: " + file_path);
-    }
-
-    {
+    } {
         // Read nodes
         file >> n_nodes;
 
-        for (int i = 0; i < n_nodes; ++i)
-        {
+        for (int i = 0; i < n_nodes; ++i) {
             Node node;
             double x, y;
             file >> node.id >> x >> y;
@@ -26,14 +21,11 @@ Graph::Graph(const string& file_path)
             nodes.push_back(node);
         }
         adjacency_matrix.resize(n_nodes, vector<bool>(n_nodes, false));
-    }
-
-    {
+    } {
         // Read edges
         int n_edges;
         file >> n_edges;
-        for (int i = 0; i < n_edges; ++i)
-        {
+        for (int i = 0; i < n_edges; ++i) {
             int from, to;
             file >> from >> to;
             adjacency_matrix[from][to] = true;
@@ -45,30 +37,26 @@ Graph::Graph(const string& file_path)
     file.close();
 }
 
-void Graph::compute_costs()
-{
-    // matrix mem(n_nodes, vector<double>(n_nodes, -1)); // memoize distance computation
+void Graph::compute_costs() {
+    costs = vector<vector<double> >(n_nodes, vector<double>(n_nodes, -1));
 
-    cout << "ADJ size: " << adjacency_matrix.size() << endl;
-    for (int i = 0; i < n_nodes; ++i)
-    {
-        for (int j = 0; j < n_nodes; ++j)
-        {
-            if (!adjacency_matrix[i][j])
-            {
+    matrix mem(n_nodes, vector<double>(n_nodes, -1)); // memoize distance computation
+    for (int i = 0; i < n_nodes; ++i) {
+        for (int j = 0; j < n_nodes; ++j) {
+            if (!adjacency_matrix[i][j]) {
                 costs[i][j] = 0;
-                // mem[i][j] = 0;
+                mem[i][j] = 0;
                 continue;
             }
 
-            // if (mem[j][i] != -1) // Assumption: cost is always positive and i->j has the same cost as j->i
-            // {
-            //     costs[i][j] = costs[j][i];
-            //     continue;
-            // }
+            if (mem[j][i] != -1) // Assumption: cost is always positive and i->j has the same cost as j->i
+            {
+                costs[i][j] = costs[j][i];
+                continue;
+            }
 
-            costs[i][j] = displacement(nodes[i].position, nodes[j].position);
-            // mem[i][j] = costs[i][j];
+            costs[i][j] = euclidean_dist(nodes[i].position, nodes[j].position);
+            mem[i][j] = costs[i][j];
         }
     }
 }
